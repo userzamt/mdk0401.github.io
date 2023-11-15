2735484c922309b730b5854c4251ecd477147ff4
userzamt <zamt.it@yandex.ru>
2023-11-15 20:04:02 +0300

    ПР12. Создание CSV-файла. Пасхалка

---
+++ PR/PR12/test-ip.ps1
@@ -0,0 +1,28 @@
+﻿$mask = "10.207.106."
+$file = "address.csv"
+
+Clear-Content -Path $file
+Add-Content -Path $file -Value "host,ip"
+
+for($i=1; $i -lt 255; $i++) {
+    $ip = $mask + $i
+     
+
+    if(Test-Connection $ip -Count 1 -Quiet) {
+        $name = Resolve-DnsName -Name $ip -Type PTR -ErrorAction SilentlyContinue 
+
+
+
+        if ($name) {
+            Write-Host $name.NameHost $ip 
+
+            $str = $name.NameHost + "," + $ip
+            Add-Content -Path $file -Value $str
+        }
+
+        $name = $null
+    } else {
+        Write-Host "`t $ip"
+    }
+
+}
